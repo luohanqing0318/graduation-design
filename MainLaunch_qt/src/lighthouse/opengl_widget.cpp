bdfd2ed8c943b086d75118afb31102670e6533e9
Tommy Luo <1104430890@qq.com>
2022-04-30 00:06:48 +0800

    20220430
    模拟太阳旋转

--- MainLaunch_qt/src/lighthouse/opengl_widget.cpp
+++ MainLaunch_qt/src/lighthouse/opengl_widget.cpp
@@ -5,7 +5,7 @@ const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
 
 
 unsigned int VAO, VBO, lightVAO;
-unsigned int depthMapFBO, depthMap;
+
 
 float vertices[] = {
     // positions          // normals
@@ -54,7 +54,7 @@ float vertices[] = {
 
 QVector3D light_position(3.0f, 0.0f, 0.0f);
 QVector3D light_color(1.0f, 1.0f, 1.0f);
-QVector3D object_color(1.0f, 0.5f, 0.31f);
+
 QVector3D view_auto_positon(0.0,5.0,0.0);
 
 
@@ -109,30 +109,7 @@ void OpenGL_Widget::initializeGL()
       if(!success) qDebug()<<"ERR:"<<m_light_shaderProgram.log();
       m_light_mesh = ProcessMesh();
 
-//      m_depthMap_shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex,":/shaders/glsl/v_depthMap.glsl");
-//      m_depthMap_shaderProgram.addShaderFromSourceFile(QOpenGLShader::Fragment,":/shaders/glsl/f_depthMap.glsl");
-//      success=m_depthMap_shaderProgram.link();
-//      if(!success) qDebug()<<"ERR:"<<m_depthMap_shaderProgram.log();
 
-
-
-
-//      glGenTextures(1, &depthMap);
-//      glBindTexture(GL_TEXTURE_2D, depthMap);
-//      glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-//      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-//      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-//      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-//      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-//      // attach depth texture as FBO's depth buffer
-//      glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-//      glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
-//      glDrawBuffer(GL_NONE);
-//      glReadBuffer(GL_NONE);
-
-//      if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-//          qDebug() << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
-//      glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebufferObject() );
 
 
 }
@@ -155,7 +132,15 @@ void OpenGL_Widget::paintGL()
        model.setToIdentity();
        view.setToIdentity();
        projection.setToIdentity();
-       //float time=m_time.elapsed()/50.0;
+       float time = m_time.elapsed()/150.0;
+       if(m_time.elapsed()/150.0 > 100)
+       {
+           time -= 99.0f;
+       }
+//       QTime time = m_time.currentTime();
+       qDebug() << time;
+
+
        projection.perspective(m_camera.Zoom,(float)width()/height(),0.1,100);
        view=m_camera.GetViewMatrix();
        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -181,60 +166,28 @@ void OpenGL_Widget::paintGL()
        // material properties
        m_shaderprogram.setUniformValue("material.shininess", 32.0f);
        m_shaderprogram.setUniformValue("model_matrix", model);
-       //m_model->Draw(m_shaderprogram);
 
-
-
-//       m_depthMap_shaderProgram.bind();
-//       // 1. render depth of scene to texture (from light's perspective)
-//       // --------------------------------------------------------------
-//       QMatrix4x4 lightProjection, lightView;
-//       QMatrix4x4 lightSpaceMatrix;
-//       float near_plane = 1.0f, far_plane = 7.5f;
-//       lightProjection.ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
-//       lightView.lookAt(light_position, QVector3D(0.0,0.0,0.0), QVector3D(0.0, 1.0, 0.0));
-//       lightSpaceMatrix = lightProjection * lightView;
-
-//       glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
-//       glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-//       glClear(GL_DEPTH_BUFFER_BIT);
-//       m_depthMap_shaderProgram.setUniformValue("lightSpaceMatrix", lightSpaceMatrix);
-
-//       model.setToIdentity();
-//       m_depthMap_shaderProgram.setUniformValue("model_matrix", model);
-//       m_model->Draw(m_depthMap_shaderProgram);
-
-//       glBindFramebuffer(GL_FRAMEBUFFER, defaultFramebufferObject());
-//       m_depthMap_shaderProgram.release();
-
-
-//       m_shaderprogram.bind();
-//       m_shaderprogram.setUniformValue("lightSpaceMatrix", lightSpaceMatrix);
-//       glViewport(0, 0, width(), height());
-//       glActiveTexture(GL_TEXTURE1);
-//       glBindTexture(GL_TEXTURE_2D, depthMap);
-//       m_shaderprogram.setUniformValue("depthMap",1);
-//       model.setToIdentity();
-//       m_shaderprogram.setUniformValue("model_matrix", model);
        m_model->Draw(m_shaderprogram);
 
 
-
-
-
        m_shaderprogram.release();
+
        m_light_shaderProgram.bind();
-           m_light_shaderProgram.setUniformValue("projection_matrix", projection);
-           m_light_shaderProgram.setUniformValue("view_matrix", view);
-           model.setToIdentity();
-           model.translate(light_position);
-           model.rotate(1.0, 1.0f, 1.0f, 0.5f);
-           model.scale(0.2);
-           m_light_shaderProgram.setUniformValue("model_matrix", model);
-           m_light_shaderProgram.setUniformValue("light_color",light_color);
+       m_light_shaderProgram.setUniformValue("projection_matrix", projection);
+       m_light_shaderProgram.setUniformValue("view_matrix", view);
+       model.setToIdentity();
 
-           m_light_mesh->Draw(m_light_shaderProgram);
 
+
+
+       model.rotate(time, 0.0f, 1.0f, 0.0f);
+       model.translate(light_position);
+       model.scale(0.2);
+       m_light_shaderProgram.setUniformValue("model_matrix", model);
+       m_light_shaderProgram.setUniformValue("light_color",light_color);
+
+       m_light_mesh->Draw(m_light_shaderProgram);
+       m_light_shaderProgram.release();
 
 }
 
@@ -312,16 +265,7 @@ Mesh *OpenGL_Widget::ProcessMesh()
        // _vertices.resize(36);
        memcpy(&_vertices[0],vertices,sizeof(vertices));
        for(int i=0;i<36;i++){
-           // Vertex vert;
-           // vert.Position[0]=vertices[i*8+0];
-           // vert.Position[1]=vertices[i*8+1];
-           // vert.Position[2]=vertices[i*8+2];
-           // vert.Normal[0]=vertices[i*8+3];
-           // vert.Normal[1]=vertices[i*8+4];
-           // vert.Normal[2]=vertices[i*8+5];
-           // vert.TexCoords[0]=vertices[i*8+6];
-           // vert.TexCoords[1]=vertices[i*8+7];
-           // _vertices.push_back(vert);
+
            _indices.push_back(i);
        }
 
