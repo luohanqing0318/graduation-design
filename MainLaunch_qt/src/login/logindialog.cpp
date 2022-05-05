d850dd0fc7dc66ddf9d43e4bfe2d481d84b2bc76
Tommy Luo <1104430890@qq.com>
2022-05-06 00:20:28 +0800

    20220506

--- MainLaunch_qt/src/login/logindialog.cpp
+++ MainLaunch_qt/src/login/logindialog.cpp
@@ -6,6 +6,8 @@ LoginDialog::LoginDialog(QWidget *parent) :
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+    ui->buttonGroup->setId(ui->radioButton_1, 0);
+    ui->buttonGroup->setId(ui->radioButton_2, 1);
 }
 
 LoginDialog::~LoginDialog()
@@ -15,7 +17,41 @@ LoginDialog::~LoginDialog()
 
 void LoginDialog::on_pushButton_clicked()
 {
-    accept();
+    QString username = ui->username_lineEdit->text();
+    QString password = ui->password_lineEdit->text();
+    QString sql;
+    if(ui->buttonGroup->checkedId() == 0)
+     {
+
+        sql = "SELECT * FROM `tenants` WHERE username = :username and `password` = :password;";
+     }else if(ui->buttonGroup->checkedId() == 1)
+    {
+
+        sql = "SELECT * FROM `landlords` WHERE username = :username and `password` = :password;";
+    }
+
+    if(username == ""  || password == "")
+    {
+         QMessageBox::about(NULL, "消息", "清完善信息");
+         return;
+    }
+    QSqlQuery query;
+    query.prepare(sql);
+    query.bindValue(":username", username);
+    query.bindValue(":password", password);
+    if(query.exec())
+    {
+        qDebug()<<"signdialog : sql select success";
+        accept();
+    }else{
+         qDebug()<<"signdialog : sql select failed";
+        QMessageBox::about(NULL, "错误", "用户名或密码错误");
+        return;
+    }
+
+
+
+
 }
 
 
